df0bf789405f6f6646c2149fb7dfe105ffdf682c
Andrey Loskutov <iloveeclipse@gmail.com>
2012-10-26 15:33:46 +0200

    DiamondOperator

---
+++ src/DiamondOperator.java
@@ -0,0 +1,32 @@
+
+/*******************************************************************************
+ * Copyright (c) 2012 Andrey Loskutov. All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *     Andrey Loskutov - initial API and implementation
+ *******************************************************************************/
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class DiamondOperator {
+    Map<Class<Number>, Map<String, List<Number>>> map;
+    {
+        // old way
+        map = new HashMap<Class<Number>, Map<String,List<Number>>>();
+
+        // "diamond" operator
+        map = new HashMap<>();
+
+        // 2 warnings:
+        // "References to generic type should be parameterized",
+        // "Expression needs unchecked conversion"
+        map = new HashMap();
+    }
+
+}
