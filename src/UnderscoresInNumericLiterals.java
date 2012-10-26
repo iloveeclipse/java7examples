6226b530a7bbe839b107e4cb9442ed4ab59ed877
Andrey Loskutov <iloveeclipse@gmail.com>
2012-10-26 15:20:18 +0200

    UnderscoresInNumericLiterals

---
+++ src/UnderscoresInNumericLiterals.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Andrey Loskutov. All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *     Andrey Loskutov - initial API and implementation
+ *******************************************************************************/
+
+public class UnderscoresInNumericLiterals {
+
+    short answer = 0b1_0_1_0_1_0;
+    int twoMonkeys = 0xAFFE_AFFE;
+    long smallestLong = 0x8000_0000_0000_0000L;
+    long somePhoneNumber = +49_7031_4357_0L;
+    double e =  2.718_281_829D;
+
+}
