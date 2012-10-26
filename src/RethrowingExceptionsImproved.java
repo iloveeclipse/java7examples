687163e464f7f02c060df646a842831e4d775532
Andrey Loskutov <iloveeclipse@gmail.com>
2012-10-26 14:50:22 +0200

    RethrowingExceptionsImproved

---
+++ src/RethrowingExceptionsImproved.java
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+/*******************************************************************************
+ * Copyright (c) 2012 Andrey Loskutov. All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *     Andrey Loskutov - initial API and implementation
+ *******************************************************************************/
+
+@SuppressWarnings("all")
+public class RethrowingExceptionsImproved {
+
+    private void oldWayRethrow(boolean notFound) throws IOException {
+        try {
+            if (notFound) {
+                throw new FileNotFoundException();
+            } else {
+                throw new MalformedURLException();
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    private void java7Rethrow(boolean notFound) throws FileNotFoundException, MalformedURLException {
+        try {
+            if (notFound) {
+                throw new FileNotFoundException();
+            } else {
+                throw new MalformedURLException();
+            }
+        } catch (IOException e) {
+            throw e; // 1.6 compiler error: "unhandled exception type IOException"
+        }
+    }
+}
