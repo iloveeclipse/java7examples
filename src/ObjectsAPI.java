fd68195cdab663582396a9d39c3f43f8d43e7a68
Andrey Loskutov <iloveeclipse@gmail.com>
2012-10-30 16:03:07 +0100

    Added Objects, slide for rethrowing exceptions

---
+++ src/ObjectsAPI.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+
+/*******************************************************************************
+ * Copyright (c) 2012 Andrey Loskutov. All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *     Andrey Loskutov - initial API and implementation
+ *******************************************************************************/
+
+public class ObjectsAPI {
+    public static void main(String[] args) {
+        String [] array1 = {"a", "b", null};
+        String [] array2 = {"a", "b", null};
+
+        System.out.println(array1.equals(array2));
+
+        System.out.println(Objects.equals(array1, array2));
+        System.out.println(Objects.deepEquals(array1, array2));
+        System.out.println(Objects.hash(array1, array2));
+
+        array1[2] = Objects.requireNonNull(System.getProperty("undefined"), "No NULL please!");
+    }
+}
