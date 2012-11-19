32f313b6347de753800243e419768845cd7d5eeb
Andrey Loskutov <iloveeclipse@gmail.com>
2012-11-19 10:44:17 +0100

    VarArgs

---
+++ src/pitfals/VarArgs.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Andrey Loskutov. All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *     Andrey Loskutov - initial API and implementation
+ *******************************************************************************/
+package pitfals;
+
+import java.util.Arrays;
+
+public class VarArgs {
+
+    public static void main(String[] args) {
+
+        Object[] result = varargs(null);
+        printArray(result);
+
+        result = varargs((String[])null);
+        printArray(result);
+
+        result = varargs((String)null);
+        printArray(result);
+
+        result = varargs();
+        printArray(result);
+
+        result = varargs(null, "x");
+        printArray(result);
+    }
+
+    static Object[] varargs(String ... strings) {
+        return strings;
+    }
+
+    private static void printArray(Object[] result) {
+        if(result != null){
+            System.out.print(result.getClass().getName() + " :\t");
+        } else {
+            System.out.print("Null object :\t\t");
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
+}
